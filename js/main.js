99bfb8e75739a110292829aa15c8b81cd60c5c81
Kurtis Wright <Kurtiscwright@gmail.com>
2017-04-30 21:54:31 -0700

    Finished Rendering the Map
    Currently people have to go to the OSM tile to see the map fill in. This
    will need to be addressed at a later date.

--- js/main.js
+++ js/main.js
@@ -19,14 +19,14 @@ $(document).ready(function() {
       subdomains: ['otile1','otile2','otile3','otile4']
   });
 
-  var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-      subdomains: ['otile1','otile2','otile3','otile4']
+  var drawLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
   });
 
   var mapLayers = {
-      "Satellite": satLayer,
-      "Map View": drawLayer,
+      //"Satellite": satLayer,
+      //"Map View": drawLayer,
       "Open Street Maps": osm
   }
 
