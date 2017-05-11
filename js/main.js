729b699c38d7dc1573cfe20d2a94e7310c64905c
Kurtis Wright <Kurtiscwright@gmail.com>
2017-05-11 16:10:26 -0700

    Finally the Map Fully Renders
    Fixed an issue with a link to a photo and moved a ; fixing the
    unrendering map.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
   var osmUrl = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
   var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-  var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+  var osM = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
 
   var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
@@ -21,14 +21,14 @@ $(document).ready(function() {
 
   var drawLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
-  });
+    }).addTo(map);
+
 
   var mapLayers = {
-      //"Satellite": satLayer,
-      //"Map View": drawLayer,
-      "Open Street Maps": osm
-  }
+      "Satellite": satLayer,  //Doesn't seem to want to render
+      "Map View": drawLayer,
+      "Open Street Maps": osM
+  };
 
   L.control.layers(mapLayers).addTo(map);
   satLayer.addTo(map);
